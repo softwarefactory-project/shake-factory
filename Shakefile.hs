b8371ac877c54d3987e1d3f7bfb07029e512f3c3
Tristan Cacqueray <tdecacqu@redhat.com>
2020-09-16 19:32:09 +0000

    Add container functions and update building rules
    Change-Id: I691f29058258718ef3b9e9f267f334ae4b711146

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,57 +1,37 @@
-import Data.List (intercalate)
-import Data.Maybe (fromMaybe)
-import qualified Data.Text as T
 import Development.Shake
-import Development.Shake.Command
-import Development.Shake.FilePath
-import Development.Shake.Util
-import qualified Dhall
 import ShakeFactory
+import ShakeFactory.Container
 import ShakeFactory.Dhall
-
-publishContainer :: Action ()
-publishContainer = do
-  gitVer <- readFile' "build/container"
-  cmd_ "podman push" imageRef
-  cmd_ "podman push" (imageRef <> ":" <> gitVer)
-
-buildContainer :: Action ()
-buildContainer = do
-  need ["build/Containerfile"]
-  cwd <- getEnvWithDefault "." "PWD"
-  cmd_ "podman build -v" (cwd <> ":/usr/src/shake-factory:Z") "-t" imageRef "-f Containerfile build"
-  Stdout gitVerInfo <- command [] "git" ["log", "--oneline", "-n", "1"]
-  let gitVer = head (words gitVerInfo)
-  cmd_ "podman tag" imageRef (imageRef <> ":" <> gitVer)
-  writeFile' "build/container" gitVer
 
 imageRef :: String
 imageRef = "quay.io/software-factory/shake-factory"
 
-containerfileDhallPackage =
-  intercalate
-    " ? "
-    [ "env:DHALL_CONTAINERFILE",
-      "~/src/softwarefactory-project.io/software-factory/dhall-containerfile",
-      "https://softwarefactory-project.io/cgit/software-factory/dhall-containerfile/plain/package.dhall"
-    ]
+containerFile :: String
+containerFile = "build/Containerfile"
+
+builderRef :: String
+builderRef = "quay.io/software-factory/stack-builder"
+
+builderFile :: String
+builderFile = "build/Containerfile-haskell-builder"
 
 generateContainerFile :: Action ()
 generateContainerFile = do
   need ["Containerfile.dhall"]
-  containerFile <- liftIO $ T.unpack <$> Dhall.input Dhall.strictText (T.pack (renderContainerfile <> "./Containerfile.dhall"))
-  writeFile' "build/Containerfile" containerFile
-  where
-    renderContainerfile = "( " <> containerfileDhallPackage <> " ).render "
+  setEnvFromAction projectVersion "GIT_REV"
+  dhallContainerAction "./Containerfile.dhall" "0.1.0" containerFile
 
 main :: IO ()
 main = shakeMain $ do
   want ["install", ".zuul.yaml"]
   ".zuul.yaml" %> dhallZuulAction "(./.sf.dhall).zuul"
-  "build/Containerfile" %> const generateContainerFile
-  "build/container" %> const buildContainer
-  phony imageRef publishContainer
+  phony "build/Containerfile" generateContainerFile
+  phony "container" (generateContainerFile >> buildContainer containerFile imageRef)
+  phony imageRef (publishContainer imageRef)
   phony "test" cabalTest
   phony "install" $ need ["test"] >> cabalInstallLib "lib:shake-factory dhall shake shake-dhall text"
   cabalDocs
   cleanRules
+  -- Builder container rules
+  phony "builder-container" (dhallContainerAction "(./package.dhall).Container.Builder" "0.1.0" builderFile >> buildContainer builderFile builderRef)
+  phony builderRef (publishContainer builderRef)
